c6593512e57c0b9b35c084be8f6e19ba21097d58
lykmapipo <lallyelias87@gmail.com>
2018-06-14 19:23:25 +0300

    Re-build docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-06-12T07:23:10.707Z",
+    "time": "2018-06-14T16:22:54.892Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
