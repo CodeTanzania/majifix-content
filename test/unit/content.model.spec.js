d5a0d6ad6cc689da7c571bd23525b7e70417f476
rijkerd <richardaggrey7@gmail.com>
2019-09-10 16:01:47 +0300

    test(unit): normalize unit test

--- test/unit/content.model.spec.js
+++ test/unit/content.model.spec.js
@@ -1,11 +1,9 @@
 import { expect } from '@lykmapipo/mongoose-test-helpers';
 import Content from '../../src/content.model';
 
-describe('Content', () => {
-  describe('Statics', () => {
-    it('should expose model name as constant', () => {
-      expect(Content.MODEL_NAME).to.exist;
-      expect(Content.MODEL_NAME).to.be.equal('Content');
-    });
+describe('Content Statics', () => {
+  it('should expose model name as constant', () => {
+    expect(Content.MODEL_NAME).to.exist;
+    expect(Content.MODEL_NAME).to.be.equal('Content');
   });
 });
