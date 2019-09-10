4c4330e28f252926b8ab591fb2cf02e8e089665f
rijkerd <richardaggrey7@gmail.com>
2019-09-10 16:31:50 +0300

    test(integration): improve integration normality

--- test/integration/content.delete.spec.js
+++ test/integration/content.delete.spec.js
@@ -2,71 +2,78 @@ import { Jurisdiction } from '@codetanzania/majifix-jurisdiction';
 import { expect, clear, create } from '@lykmapipo/mongoose-test-helpers';
 import { Content } from '../../src';
 
-describe('Content', () => {
+describe('Content static delete', () => {
+  before(done => clear(Content, Jurisdiction, done));
+
   const jurisdiction = Jurisdiction.fake();
 
-  before(done => clear(Content, Jurisdiction, done));
   before(done => create(jurisdiction, done));
 
-  describe('static delete', () => {
-    let content;
+  let content;
 
-    before(done => {
-      content = Content.fake();
-      content.jurisdiction = jurisdiction;
-      content.post((error, created) => {
-        content = created;
-        done(error, created);
-      });
-    });
-
-    it('should be able to delete', done => {
-      Content.del(content._id, (error, deleted) => {
-        expect(error).to.not.exist;
-        expect(deleted).to.exist;
-        expect(deleted._id).to.eql(content._id);
-        done(error, deleted);
-      });
-    });
-
-    it('should throw if not exists', done => {
-      Content.del(content._id, (error, deleted) => {
-        expect(error).to.exist;
-        // expect(error.status).to.exist;
-        expect(error.name).to.be.equal('DocumentNotFoundError');
-        expect(deleted).to.not.exist;
-        done();
-      });
+  before(done => {
+    content = Content.fake();
+    content.jurisdiction = jurisdiction;
+    content.post((error, created) => {
+      content = created;
+      done(error, created);
     });
   });
 
-  describe('instance delete', () => {
-    let content;
-
-    before(done => {
-      const fake = Content.fake();
-      fake.post((error, created) => {
-        content = created;
-        done(error, created);
-      });
+  it('should be able to delete', done => {
+    Content.del(content._id, (error, deleted) => {
+      expect(error).to.not.exist;
+      expect(deleted).to.exist;
+      expect(deleted._id).to.eql(content._id);
+      done(error, deleted);
     });
+  });
 
-    it('should be able to delete', done => {
-      content.del((error, deleted) => {
-        expect(error).to.not.exist;
-        expect(deleted).to.exist;
-        expect(deleted._id).to.eql(content._id);
-        done(error, deleted);
-      });
+  it('should throw if not exists', done => {
+    Content.del(content._id, (error, deleted) => {
+      expect(error).to.exist;
+      // expect(error.status).to.exist;
+      expect(error.name).to.be.equal('DocumentNotFoundError');
+      expect(deleted).to.not.exist;
+      done();
     });
+  });
 
-    it('should throw if not exists', done => {
-      content.del((error, deleted) => {
-        expect(error).to.not.exist;
-        expect(deleted).to.exist;
-        expect(deleted._id).to.eql(content._id);
-        done();
-      });
+  after(done => clear(Content, Jurisdiction, done));
+});
+
+describe('Content instance delete', () => {
+  before(done => clear(Content, Jurisdiction, done));
+
+  const jurisdiction = Jurisdiction.fake();
+
+  before(done => create(jurisdiction, done));
+
+  let content;
+
+  before(done => {
+    const fake = Content.fake();
+    fake.post((error, created) => {
+      content = created;
+      done(error, created);
+    });
+  });
+
+  it('should be able to delete', done => {
+    content.del((error, deleted) => {
+      expect(error).to.not.exist;
+      expect(deleted).to.exist;
+      expect(deleted._id).to.eql(content._id);
+      done(error, deleted);
+    });
+  });
+
+  it('should throw if not exists', done => {
+    content.del((error, deleted) => {
+      expect(error).to.not.exist;
+      expect(deleted).to.exist;
+      expect(deleted._id).to.eql(content._id);
+      done();
     });
   });
 
--- test/integration/content.get.spec.js
+++ test/integration/content.get.spec.js
@@ -3,118 +3,118 @@ import { Jurisdiction } from '@codetanzania/majifix-jurisdiction';
 import { expect, clear, create } from '@lykmapipo/mongoose-test-helpers';
 import { Content } from '../../src';
 
-describe('Content', () => {
+describe('Content get', () => {
+  before(done => clear(Content, Jurisdiction, done));
+
   const jurisdiction = Jurisdiction.fake();
 
-  before(done => clear(Content, Jurisdiction, done));
   before(done => create(jurisdiction, done));
 
-  describe('get', () => {
-    let contents = Content.fake(32);
-    contents = _.map(contents, content => {
-      content.set({ jurisdiction });
-      return content;
+  let contents = Content.fake(32);
+
+  contents = _.map(contents, content => {
+    content.set({ jurisdiction });
+    return content;
+  });
+
+  before(done => create(...contents, done));
+
+  it('should be able to get without options', done => {
+    Content.get((error, results) => {
+      expect(error).to.not.exist;
+      expect(results).to.exist;
+      expect(results.data).to.exist;
+      expect(results.data).to.have.length(10);
+      expect(results.total).to.exist;
+      expect(results.total).to.be.equal(32);
+      expect(results.limit).to.exist;
+      expect(results.limit).to.be.equal(10);
+      expect(results.skip).to.exist;
+      expect(results.skip).to.be.equal(0);
+      expect(results.page).to.exist;
+      expect(results.page).to.be.equal(1);
+      expect(results.pages).to.exist;
+      expect(results.pages).to.be.equal(4);
+      expect(results.lastModified).to.exist;
+      expect(_.maxBy(results.data, 'updatedAt').updatedAt).to.be.at.most(
+        results.lastModified
+      );
+      done(error, results);
     });
+  });
 
-    before(done => create(...contents, done));
-
-    it('should be able to get without options', done => {
-      Content.get((error, results) => {
-        expect(error).to.not.exist;
-        expect(results).to.exist;
-        expect(results.data).to.exist;
-        expect(results.data).to.have.length(10);
-        expect(results.total).to.exist;
-        expect(results.total).to.be.equal(32);
-        expect(results.limit).to.exist;
-        expect(results.limit).to.be.equal(10);
-        expect(results.skip).to.exist;
-        expect(results.skip).to.be.equal(0);
-        expect(results.page).to.exist;
-        expect(results.page).to.be.equal(1);
-        expect(results.pages).to.exist;
-        expect(results.pages).to.be.equal(4);
-        expect(results.lastModified).to.exist;
-        expect(_.maxBy(results.data, 'updatedAt').updatedAt).to.be.at.most(
-          results.lastModified
-        );
-        done(error, results);
-      });
+  it('should be able to get with options', done => {
+    const options = { page: 1, limit: 20 };
+    Content.get(options, (error, results) => {
+      expect(error).to.not.exist;
+      expect(results).to.exist;
+      expect(results.data).to.exist;
+      expect(results.data).to.have.length(20);
+      expect(results.total).to.exist;
+      expect(results.total).to.be.equal(32);
+      expect(results.limit).to.exist;
+      expect(results.limit).to.be.equal(20);
+      expect(results.skip).to.exist;
+      expect(results.skip).to.be.equal(0);
+      expect(results.page).to.exist;
+      expect(results.page).to.be.equal(1);
+      expect(results.pages).to.exist;
+      expect(results.pages).to.be.equal(2);
+      expect(results.lastModified).to.exist;
+      expect(_.maxBy(results.data, 'updatedAt').updatedAt).to.be.at.most(
+        results.lastModified
+      );
+      done(error, results);
     });
+  });
 
-    it('should be able to get with options', done => {
-      const options = { page: 1, limit: 20 };
-      Content.get(options, (error, results) => {
-        expect(error).to.not.exist;
-        expect(results).to.exist;
-        expect(results.data).to.exist;
-        expect(results.data).to.have.length(20);
-        expect(results.total).to.exist;
-        expect(results.total).to.be.equal(32);
-        expect(results.limit).to.exist;
-        expect(results.limit).to.be.equal(20);
-        expect(results.skip).to.exist;
-        expect(results.skip).to.be.equal(0);
-        expect(results.page).to.exist;
-        expect(results.page).to.be.equal(1);
-        expect(results.pages).to.exist;
-        expect(results.pages).to.be.equal(2);
-        expect(results.lastModified).to.exist;
-        expect(_.maxBy(results.data, 'updatedAt').updatedAt).to.be.at.most(
-          results.lastModified
-        );
-        done(error, results);
-      });
+  it('should be able to search with options', done => {
+    const options = { filter: { q: contents[0].title.en } };
+    Content.get(options, (error, results) => {
+      expect(error).to.not.exist;
+      expect(results).to.exist;
+      expect(results.data).to.exist;
+      expect(results.data).to.have.length.of.at.least(1);
+      expect(results.total).to.exist;
+      expect(results.total).to.be.at.least(1);
+      expect(results.limit).to.exist;
+      expect(results.limit).to.be.equal(10);
+      expect(results.skip).to.exist;
+      expect(results.skip).to.be.equal(0);
+      expect(results.page).to.exist;
+      expect(results.page).to.be.equal(1);
+      expect(results.pages).to.exist;
+      expect(results.pages).to.be.equal(1);
+      expect(results.lastModified).to.exist;
+      expect(_.maxBy(results.data, 'updatedAt').updatedAt).to.be.at.most(
+        results.lastModified
+      );
+      done(error, results);
     });
+  });
 
-    it('should be able to search with options', done => {
-      const options = { filter: { q: contents[0].title.en } };
-      Content.get(options, (error, results) => {
-        expect(error).to.not.exist;
-        expect(results).to.exist;
-        expect(results.data).to.exist;
-        expect(results.data).to.have.length.of.at.least(1);
-        expect(results.total).to.exist;
-        expect(results.total).to.be.at.least(1);
-        expect(results.limit).to.exist;
-        expect(results.limit).to.be.equal(10);
-        expect(results.skip).to.exist;
-        expect(results.skip).to.be.equal(0);
-        expect(results.page).to.exist;
-        expect(results.page).to.be.equal(1);
-        expect(results.pages).to.exist;
-        expect(results.pages).to.be.equal(1);
-        expect(results.lastModified).to.exist;
-        expect(_.maxBy(results.data, 'updatedAt').updatedAt).to.be.at.most(
-          results.lastModified
-        );
-        done(error, results);
-      });
-    });
-
-    it('should parse filter options', done => {
-      const options = { filter: { 'title.en': contents[0].title.en } };
-      Content.get(options, (error, results) => {
-        expect(error).to.not.exist;
-        expect(results).to.exist;
-        expect(results.data).to.exist;
-        expect(results.data).to.have.length.of.at.least(1);
-        expect(results.total).to.exist;
-        expect(results.total).to.be.at.least(1);
-        expect(results.limit).to.exist;
-        expect(results.limit).to.be.equal(10);
-        expect(results.skip).to.exist;
-        expect(results.skip).to.be.equal(0);
-        expect(results.page).to.exist;
-        expect(results.page).to.be.equal(1);
-        expect(results.pages).to.exist;
-        expect(results.pages).to.be.equal(1);
-        expect(results.lastModified).to.exist;
-        expect(_.maxBy(results.data, 'updatedAt').updatedAt).to.be.at.most(
-          results.lastModified
-        );
-        done(error, results);
-      });
+  it('should parse filter options', done => {
+    const options = { filter: { 'title.en': contents[0].title.en } };
+    Content.get(options, (error, results) => {
+      expect(error).to.not.exist;
+      expect(results).to.exist;
+      expect(results.data).to.exist;
+      expect(results.data).to.have.length.of.at.least(1);
+      expect(results.total).to.exist;
+      expect(results.total).to.be.at.least(1);
+      expect(results.limit).to.exist;
+      expect(results.limit).to.be.equal(10);
+      expect(results.skip).to.exist;
+      expect(results.skip).to.be.equal(0);
+      expect(results.page).to.exist;
+      expect(results.page).to.be.equal(1);
+      expect(results.pages).to.exist;
+      expect(results.pages).to.be.equal(1);
+      expect(results.lastModified).to.exist;
+      expect(_.maxBy(results.data, 'updatedAt').updatedAt).to.be.at.most(
+        results.lastModified
+      );
+      done(error, results);
     });
   });
 
